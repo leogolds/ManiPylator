01bb08d1cc615c1f2bbd5643a3b99599ebbd1971
Leo Goldstien <leogoldstien@gmail.com>
2025-02-25 20:43:54 -0500

    Base integration of rtb and genesis

---
+++ manipylator/base.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+import roboticstoolbox as rtb
+import genesis as gs
+
+gs.init(backend=gs.cpu)
+# gs.init(backend=gs.gpu)
+
+
+class Visualizer:
+    def __init__(self, urdf_path: Path):
+        try:
+            self.morph = gs.morphs.URDF(file=str(urdf_path))
+
+        except Exception as e:
+            raise e
+
+
+class Robot:
+    def __init__(self, urdf_path: Path):
+        try:
+            self.model = rtb.Robot.URDF(urdf_path)
+            self.visualizer = Visualizer(urdf_path)
+        except Exception as e:
+            raise e
