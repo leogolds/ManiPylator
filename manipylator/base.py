ed9ada9ee0e50778beda1c1e183e034f31626c5f
Leo Goldstien <leogoldstien@gmail.com>
2025-03-16 23:42:21 -0400

    basic control example works!

--- manipylator/base.py
+++ manipylator/base.py
@@ -1,19 +1,50 @@
 from pathlib import Path
 
 import roboticstoolbox as rtb
-import genesis as gs
 
-# gs.init(backend=gs.cpu)
-gs.init(backend=gs.gpu)
+import genesis as gs
 
 
 class Visualizer:
     def __init__(self, urdf_path: Path):
+        # gs.init(backend=gs.cpu)
+        gs.init(backend=gs.gpu)
+
         try:
-            self.morph = gs.morphs.URDF(file=str(urdf_path))
+            self.morph = gs.morphs.URDF(
+                file=str(urdf_path),
+                fixed=True,
+                pos=(0, 0, 0),
+            )
+            self.scene, self.robot = self._init_scene()
 
         except Exception as e:
             raise e
+
+    def _init_scene(self):
+        scene = gs.Scene(
+            viewer_options=gs.options.ViewerOptions(
+                camera_pos=(3, -1, 1.5),
+                camera_lookat=(0.0, 0.0, 0.5),
+                camera_fov=30,
+                max_FPS=60,
+            ),
+            sim_options=gs.options.SimOptions(
+                dt=0.01,
+                substeps=4,  # for more stable grasping contact
+            ),
+            show_viewer=True,
+            show_FPS=False,
+        )
+
+        plane = scene.add_entity(
+            gs.morphs.Plane(),
+        )
+        robot = scene.add_entity(self.morph)
+
+        scene.build(compile_kernels=True)
+
+        return scene, robot
 
 
 class Robot:
